57882cb7a5a34c3b5c0b11a6fe9f56dfc9375ace
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-20 15:53:14 -0200

    primeir commit

---
+++ app/src/main/java/com/dmagdaleno/ceep/ui/activity/ListaNotasActivity.kt
@@ -0,0 +1,13 @@
+package com.dmagdaleno.ceep.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dmagdaleno.ceep.R
+
+class ListaNotasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_notas)
+    }
+}
