4c480c47f91958c7ec700f3a56cd2b04da2476a3
dmagdaleno <diegommagdaleno@gmail.com>
2019-01-21 09:56:14 -0200

    implementa todos os métodos do adapter

--- app/src/main/java/com/dmagdaleno/ceep/ui/rv/adapter/ListaNotasAdapter.kt
+++ app/src/main/java/com/dmagdaleno/ceep/ui/rv/adapter/ListaNotasAdapter.kt
@@ -1,19 +1,33 @@
 package com.dmagdaleno.ceep.ui.rv.adapter
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView.Adapter
 import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.dmagdaleno.ceep.R
 import com.dmagdaleno.ceep.model.Nota
+import kotlinx.android.synthetic.main.item_nota.view.*
 
-class ListaNotasAdapter(private val notas: List<Nota>) : Adapter<ViewHolder>() {
-    
+class ListaNotasAdapter(
+        private val context: Context,
+        private val notas: List<Nota>) : Adapter<ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false)
+        return NotaViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val nota = notas[position]
+        with(holder.itemView) {
+            item_nota_titulo.text = nota.titulo
+            item_nota_descricao.text = nota.descricao
+        }
     }
 
     override fun getItemCount() = notas.size
+
+    class NotaViewHolder(item: View): ViewHolder(item)
 }
