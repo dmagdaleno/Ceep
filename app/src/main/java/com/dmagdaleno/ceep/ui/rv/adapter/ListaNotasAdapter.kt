0770c39292cd293600e05bd6c7fef583264110d8
dmagdaleno <diegommagdaleno@gmail.com>
2019-01-21 21:56:16 -0200

    boas praticas da RecyclerView

--- app/src/main/java/com/dmagdaleno/ceep/ui/rv/adapter/ListaNotasAdapter.kt
+++ app/src/main/java/com/dmagdaleno/ceep/ui/rv/adapter/ListaNotasAdapter.kt
@@ -6,28 +6,34 @@ import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.dmagdaleno.ceep.R
 import com.dmagdaleno.ceep.model.Nota
 import kotlinx.android.synthetic.main.item_nota.view.*
 
 class ListaNotasAdapter(
         private val context: Context,
-        private val notas: List<Nota>) : Adapter<ViewHolder>() {
+        private val notas: List<Nota>) : Adapter<ListaNotasAdapter.NotaViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotaViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false)
         return NotaViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: NotaViewHolder, position: Int) {
         val nota = notas[position]
-        with(holder.itemView) {
-            item_nota_titulo.text = nota.titulo
-            item_nota_descricao.text = nota.descricao
-        }
+        holder.vincula(nota)
     }
 
     override fun getItemCount() = notas.size
 
-    class NotaViewHolder(item: View): ViewHolder(item)
+    class NotaViewHolder(item: View): ViewHolder(item) {
+        private val titulo = item.item_nota_titulo
+        private val descricao = item.item_nota_descricao
+
+        fun vincula(nota: Nota) {
+            titulo.text = nota.titulo
+            descricao.text = nota.descricao
+        }
+    }
 }
