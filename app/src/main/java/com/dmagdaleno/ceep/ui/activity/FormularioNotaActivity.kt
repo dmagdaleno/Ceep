829a5c9a02860605125516fff425d71d635d8845
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-22 22:39:16 -0200

    salva nova nota

--- app/src/main/java/com/dmagdaleno/ceep/ui/activity/FormularioNotaActivity.kt
+++ app/src/main/java/com/dmagdaleno/ceep/ui/activity/FormularioNotaActivity.kt
@@ -2,11 +2,36 @@ package com.dmagdaleno.ceep.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.dmagdaleno.ceep.R
+import com.dmagdaleno.ceep.dao.NotaDAO
+import com.dmagdaleno.ceep.model.Nota
+import kotlinx.android.synthetic.main.activity_formulario_nota.*
 
 class FormularioNotaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_nota)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_formulario_salva_nota, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_formulario_ic_salva_nota -> {
+                val titulo = formulario_nota_titulo.text.toString()
+                val descricao = formulario_nota_descricao.text.toString()
+                val nota = Nota(titulo, descricao)
+                NotaDAO().insere(nota)
+                finish()
+            }
+            else -> { }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
