2e6405d9c6e662aa90f3dd6278c667250712663c
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-23 21:33:50 -0200

    adiciona nota ao adapter

--- app/src/main/java/com/dmagdaleno/ceep/ui/rv/adapter/ListaNotasAdapter.kt
+++ app/src/main/java/com/dmagdaleno/ceep/ui/rv/adapter/ListaNotasAdapter.kt
@@ -6,14 +6,13 @@ import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.dmagdaleno.ceep.R
 import com.dmagdaleno.ceep.model.Nota
 import kotlinx.android.synthetic.main.item_nota.view.*
 
 class ListaNotasAdapter(
         private val context: Context,
-        private val notas: List<Nota>) : Adapter<ListaNotasAdapter.NotaViewHolder>() {
+        private val notas: MutableList<Nota>) : Adapter<ListaNotasAdapter.NotaViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotaViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false)
@@ -39,5 +38,10 @@ class ListaNotasAdapter(
             titulo.text = nota.titulo
             descricao.text = nota.descricao
         }
+    }
+
+    fun adiciona(nota: Nota){
+        this.notas.add(nota)
+        this.notifyDataSetChanged()
     }
 }
