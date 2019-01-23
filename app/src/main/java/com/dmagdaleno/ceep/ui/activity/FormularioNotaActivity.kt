ee4e8544e1ef89e8f92869f866b3738b4165cd0e
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-22 22:01:53 -0200

    adiciona nova activity para inserção de notas

---
+++ app/src/main/java/com/dmagdaleno/ceep/ui/activity/FormularioNotaActivity.kt
@@ -0,0 +1,12 @@
+package com.dmagdaleno.ceep.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioNotaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_nota)
+    }
+}
