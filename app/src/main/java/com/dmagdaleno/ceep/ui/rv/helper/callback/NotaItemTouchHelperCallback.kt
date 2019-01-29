c9c4cfd6663fdec038f477a3116db5bf79cb89bd
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-28 23:20:20 -0200

    implementa metodo para remover item no swype

--- app/src/main/java/com/dmagdaleno/ceep/ui/rv/helper/callback/NotaItemTouchHelperCallback.kt
+++ app/src/main/java/com/dmagdaleno/ceep/ui/rv/helper/callback/NotaItemTouchHelperCallback.kt
@@ -2,18 +2,24 @@ package com.dmagdaleno.ceep.ui.rv.helper.callback
 
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
+import com.dmagdaleno.ceep.dao.NotaDAO
+import com.dmagdaleno.ceep.ui.rv.adapter.ListaNotasAdapter
 
-class NotaItemTouchHelperCallback: ItemTouchHelper.Callback() {
+class NotaItemTouchHelperCallback(
+        private val adapter: ListaNotasAdapter): ItemTouchHelper.Callback() {
+
     override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
         val marcadores = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
         return makeMovementFlags(0, marcadores)
     }
 
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return false
     }
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val posicao = viewHolder.adapterPosition
+        //NotaDAO().remove(posicao)
+        adapter.remove(posicao)
     }
 }
