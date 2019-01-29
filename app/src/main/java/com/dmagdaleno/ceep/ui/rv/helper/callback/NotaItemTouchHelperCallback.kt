f27318cad44e18b950a56dc9df887da3b0c88dd5
dmagdaleno <diegommagdaleno@gmail.com>
2019-01-29 16:26:40 -0200

    arrastar item

--- app/src/main/java/com/dmagdaleno/ceep/ui/rv/helper/callback/NotaItemTouchHelperCallback.kt
+++ app/src/main/java/com/dmagdaleno/ceep/ui/rv/helper/callback/NotaItemTouchHelperCallback.kt
@@ -2,15 +2,15 @@ package com.dmagdaleno.ceep.ui.rv.helper.callback
 
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
-import com.dmagdaleno.ceep.dao.NotaDAO
 import com.dmagdaleno.ceep.ui.rv.adapter.ListaNotasAdapter
 
 class NotaItemTouchHelperCallback(
         private val adapter: ListaNotasAdapter): ItemTouchHelper.Callback() {
 
     override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
-        val marcadores = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-        return makeMovementFlags(0, marcadores)
+        val marcadoresDeslize = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        val marcadoresArrastar = ItemTouchHelper.DOWN or ItemTouchHelper.UP or marcadoresDeslize
+        return makeMovementFlags(marcadoresArrastar, marcadoresDeslize)
     }
 
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
